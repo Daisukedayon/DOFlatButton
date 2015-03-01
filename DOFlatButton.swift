24d131a78312e07fef50cec0b3ee38349106dcfc
岡　大輔（Daisuke Oka) <daisuke_oka@nanosoftware.biz>
2015-03-01 15:04:07 +0900

    制作途中

--- DOFlatButton.swift
+++ DOFlatButton.swift
@@ -13,14 +13,28 @@ import UIKit
 
 class DOFlatButton :UIButton
 {
-    internal var faceColor:UIColor
-    internal var sideColor:UIColor
-    internal var borderColor:UIColor
+    internal var faceColor:NSMutableDictionary?
+    internal var sideColor:UIColor?{
+        set{
+            self.sideColor = newValue
+        }
+        get{
+            return self.faceColorForState(self.state)
+        }
+    }
+    internal var borderColor:UIColor?{
+        set{
+            self.setBorderColor(self.borderColor!, state: state)
+        }
+        get{
+            return self.borderColorForState(state)
+        }
+    }
     
-    internal var radius:CGFloat
-    internal var margin:CGFloat
-    internal var depth:CGFloat
-    internal var borderWidth:CGFloat
+    internal var radius:CGFloat?
+    internal var margin:CGFloat?
+    internal var depth:CGFloat?
+    internal var borderWidth:CGFloat?
     
     internal func setFaceColor(faceColor:UIColor,state:UIControlState)
     {
@@ -55,11 +69,19 @@ class DOFlatButton :UIButton
     }
     
     required convenience init(coder aDecoder: NSCoder) {
+        self.init()
+        self.dofb_init()
+    }
+    
+    override init(){
+        super.init()
         self.dofb_init()
     }
     
     func dofb_init()
     {
+        self.faceColor = NSMutableDictionary
+        
         self.faceColor = UIColor(red: 0.333, green: 0.631, blue: 0.851, alpha: 1.0)
         self.sideColor = UIColor(red: 0.310, green: 0.498, blue: 0.702, alpha: 1.0)
         self.borderColor = UIColor(red: 0.310, green: 0.498, blue: 0.702, alpha: 1.0)
@@ -75,11 +97,80 @@ class DOFlatButton :UIButton
     override func layoutSubviews()
     {
         var frame:CGRect = self.titleLabel!.frame
-        frame.origin.y = frame.origin.y - self.margin/2
+        frame.origin.y = frame.origin.y - self.margin!/2
         
-        var imageViewFrame:CGRect = self.imageView?.frame
+        var imageViewFrame:CGRect = self.imageView!.frame
+        imageViewFrame.origin.y = imageViewFrame.origin.y - self.margin!/2
         
+        if(self.state == UIControlState.Selected || self.state == UIControlState.Highlighted){
+            frame.origin.y = frame.origin.y + self.depth!
+            imageViewFrame.origin.y = imageViewFrame.origin.y + self.depth!
+        }
+        
+        self.titleLabel?.frame = frame
+        self.imageView?.frame = imageViewFrame
+    }
+    
+    override var frame:CGRect{
+        get{
+            return super.frame
+        }
+        set{
+            super.frame = newValue
+            self.setNeedsDisplay()
+        }
+    }
+    
+    var Highlighted:Bool{
+        set{
+            super.highlighted = newValue
+            self.setNeedsDisplay()
+        }
+        get{
+            return super.highlighted
+        }
         
     }
+    override var selected:Bool{
+        set{
+            super.selected = newValue
+            self.setNeedsDisplay()
+        }
+        get{
+            return self.selected
+        }
+    }
+    
+    override var enabled:Bool{
+        set{
+            super.enabled = newValue
+            self.setNeedsDisplay()
+        }
+        get{
+            return super.highlighted
+        }
+    }
+    
+    func setFaceColor(faceColor:UIColor){
+        self.setFaceColor(faceColor, state: UIControlState.Normal)
+        self.setFaceColor(faceColor, state: UIControlState.Highlighted)
+        self.setFaceColor(faceColor, state: UIControlState.Selected)
+        self.setFaceColor(faceColor, state: UIControlState.Disabled)
+    }
+    
+    func setBorderColor(borderColor:UIColor){
+        self.setBorderColor(borderColor, state: UIControlState.Normal)
+        self.setBorderColor(borderColor, state: UIControlState.Highlighted)
+        self.setBorderColor(borderColor, state: UIControlState.Selected)
+        self.setBorderColor(borderColor, state: UIControlState.Disabled)
+    }
+    
+    func setFaceColor(surfaceColor:UIColor,state:UIControlState)
+    {
+        self.faceColorForState[state] = surfaceColor
+    }
+    
+    
+    
 }
 
